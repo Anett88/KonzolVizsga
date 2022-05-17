98f46465e008b289062e43b63fc32f36000f85ac
Anett88 <sz.anett888@gmail.com>
2022-05-17 15:08:47 +0200

    3.commit

--- src/raktarprojekt/RaktarProjekt.java
+++ src/raktarprojekt/RaktarProjekt.java
@@ -1,12 +1,43 @@
 
 package raktarprojekt;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class RaktarProjekt {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        ArrayList<Elelmiszer> lista = null;
+        Raktar raktar =new Raktar(lista);
+        
+        
+        
+        
+        try {
+            
+            ObjectOutputStream objKi = new ObjectOutputStream(new FileOutputStream("raktar.bin"));
+            objKi.writeObject(raktar);
+            objKi.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(RaktarProjekt.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (IOException ex) {
+            Logger.getLogger(RaktarProjekt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+         
     }
+ 
     
-}
+    
+    
+
